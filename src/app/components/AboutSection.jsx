ceee41379901e4f76bf44d9dec9b08f5936885e8
shgonzals <sh.gonzal.s@gmail.com>
2023-10-14 20:49:18 +0200

    create about me section

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,83 @@
+"use client"
+import React, { useTransition, useState} from 'react';
+import Image from 'next/image';
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: 'Skills',
+    id: 'skills',
+    content: (
+      <ul className='list-disc pl-2'>
+        <li>Java 8</li>
+        <li>Hibernate/JPA</li>
+        <li>API/REST</li>
+        <li>Oracle/SQL - PL/SQL</li>
+        <li>GIT</li>
+        <li>Angular</li>
+        <li>Sencha ExtJS</li>
+      </ul>
+    )
+  },{
+    title: 'Certificaciones',
+    id: 'certifications',
+    content: (
+      <ul className='list-disc pl-2'>
+        <li>Angular Level 2 Certification - Interstate 21, LLC (12/2022) [ID: 3L14N3T]</li>
+        <li>Spring Boot-JPA - Desfufor S.L. (09/2022)</li>
+        <li>Errores recurrentes en la programación SW (20h) - 2018</li>
+        <li>Oracle11g Lenguaje PL/SQL II (12h) - 2018</li>
+      </ul>
+    )
+  },{
+    title: 'Estudios',
+    id: 'education',
+    content: (
+      <ul className='list-disc pl-2'>
+        <li>Técnico Superior en Desarrollo de Aplicaciones Multiplataforma (2015 - 2017)</li>
+      </ul>
+    )
+  }
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className='text-white'>
+        <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
+            <Image src="/images/about.jpg" className='rounded-full' width={500} height={500} alt=''/>
+            <div className='mt-4 md:mt-0 text-left flex flex-col h-full'>
+              <h2 className='text-4xl font-semibold text-white mb-4'>Sobre mi</h2>
+              <div className='text-base lg:text-lg'>
+                <p>
+                Actualmente desarrollo funcionalidades como backend con Java 8 y Spring Batch para una aplicación refactorizada, en la cual también participo en el análisis de los casos de uso a realizar, basándonos en la aplicación de origen (CA Plex).
+                </p>
+                <br/>
+                <p>
+                  Me considero una persona con gran capacidad resolutiva, dinámica y con adaptación a nuevos entornos y dificultades, buena compañera en el trabajo en equipo y con ganas de seguir formándome y progresando en las tecnologias del desarrollo.
+                </p>  
+              </div>
+              <div className='flex justify-start flex-row mt-8'>
+                <TabButton selectTab={() => handleTabChange("skills")} active={tab === "skills"}>{" "} Skills {" "}</TabButton>
+                <TabButton selectTab={() => handleTabChange("certifications")} active={tab === "certifications"}>{" "} Certificaciones {" "}</TabButton>
+                <TabButton selectTab={() => handleTabChange("education")} active={tab === "education"}>{" "} Estudios {" "}</TabButton>
+              </div>
+              <div className='mt-8'>
+                {TAB_DATA.find((t) => t.id === tab).content}
+              </div>
+            </div>
+        </div>
+    </section>
+   
+  )
+}
+
+export default AboutSection
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -27,8 +27,7 @@ const HeroSection = () => {
                     />
                 </h1>
                 <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lf:text-xl">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
+                Desarrolladora web de la Comunidad Valenciana con gran vocación por la programación y nuevas tecnologías.                </p>
                 <div>
                     <button className="px-6 py-3 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-400 via-purple-500 to-pink-500  hover:bg-slate-200 text-white mr-4">
                         Hire Me
