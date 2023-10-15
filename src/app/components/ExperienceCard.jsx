7ab33b27f84b64edbbdec292f1cca57da2cc327f
shgonzals <sh.gonzal.s@gmail.com>
2023-10-15 20:56:35 +0200

    update experience data

--- src/app/components/ExperienceCard.jsx
+++ src/app/components/ExperienceCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { motion } from 'framer-motion'; 
 
-const ExperienceCard = () => {
+const ExperienceCard = ({src, puesto, empresa, fechas, content, id}) => {
   return (
     <article className='text-white mt-12 flex flex-col rounded-lg items-center space-y-7 flex-shrink-0
     w-[400px] md-[400px] xl:w-[400px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40
@@ -15,29 +15,20 @@ const ExperienceCard = () => {
         whileInView={{opacity: 1, y: 0}}
         viewport={{once: true}}
         className='w-32 h-32 rounded-full xl:w-[80px] xl:h-[80px]
-        object-cover object-start'
-        src='/images/logos/angular.svg'
-        alt="test"
+        object-cover object-start bg-white'
+        src={src}
+        alt={id}
+        
         >
         </motion.img>
 
         <div className='px-0 md:px-10'>
-            <h4 className='text-4xl font-light'>CEO</h4>
-            <p className='font-bold text-2xl mt-1'>Company</p>
-            <div className='flex space-x-2 my-2'>
-                {/*
-                    TECH USED
-                */ }
-            </div>
+            <h4 className='text-2xl font-light'>{puesto}</h4>
+            <p className='font-bold text-xl mt-1'>{empresa}</p>
             <p className='uppercase py-5 text-gray-300'>
-                Fechas
-            </p>
-            <ul className='list-disc space-y-4 ml-5 text-lg'>
-                <li>Summary pointsSummary points</li>
-                <li>Summary pointsSummary points</li>
-                <li>Summary pointsSummary points</li>
-                <li>Summary pointsSummary points</li>
-            </ul>
+                {fechas}
+            </p> 
+            {content}
         </div>
     </article>
   )
--- src/app/components/ExperienceSection.jsx
+++ src/app/components/ExperienceSection.jsx
@@ -3,6 +3,80 @@ import React from 'react'
 import { motion } from 'framer-motion'; 
 import ExperienceCard from './ExperienceCard';
 
+
+const DATA = [
+    {
+        id: 1,
+        src: '/images/company/capgemini.png',
+        puesto: 'Full-Stack Developer',
+        empresa: 'Capgemini',
+        fechas: 'Julio 2021 - Presente',
+        content: (
+            <ul className='list-disc space-y-4 ml-5 text-s'>
+                <li>Desarrollo backend con Java 11 y Spring Batch</li>
+                <li>Análisis de requisitos para una aplicación refactorizada (Java) en base a los casos de uso de la aplicación origen (CA Plex)</li>
+                <li>Desarrollo de front-end (ExtJS) y back-end (Java)</li>
+                <li>Mantenimiento de aplicaciones</li>
+            </ul>
+        )
+    },{
+        id: 2,
+        src: '/images/company/oesia.png',
+        puesto: 'Full-Stack Developer',
+        empresa: 'Grupo Oesia',
+        fechas: 'Abril 2021 - Julio 2021',
+        content: (
+            <ul className='list-disc space-y-4 ml-5 text-s'>
+                <li>Desarrollo de back-end (Java 8)</li>
+                <li>Ejecución y creación de pruebas</li>
+            </ul>
+        )
+    },{
+        id: 3,
+        src: '/images/company/pfslogo.png',
+        puesto: 'Full-Stack Developer',
+        empresa: 'PFS Group',
+        fechas: 'Abril 2020 - Marzo 2021',
+        content: (
+            <ul className='list-disc space-y-4 ml-5 text-s'>
+                <li>Desarrollo de front-end (ExtJS/Angular) y back-end (Java)</li>
+                <li>Mantenimiento de aplicaciones</li>
+                <li>Ejecución de pruebas</li>
+                <li>Análisis de requisitos</li>
+                <li>Desarrollo de documentación funcional</li>
+            </ul>
+        )
+    },{
+        id: 4,
+        src: '/images/company/images.png',
+        puesto: 'Full-Stack Developer',
+        empresa: 'Cofares',
+        fechas: 'Octubre 2019 - Abril 2020',
+        content: (
+            <ul className='list-disc space-y-4 ml-5 text-s'>
+                <li>Desarrollo de front-end (ExtJS) y back-end (Java)</li>
+                <li>Mantenimiento de aplicaciones</li>
+                <li>Ejecución de pruebas</li>
+                <li>Desarrollo de documentación funcional</li>
+            </ul>
+        )
+    },{
+        id: 5,
+        src: '/images/company/minsait.png',
+        puesto: 'Full-Stack Developer',
+        empresa: 'Indra Minsait',
+        fechas: 'Marzo 2017 - Septiembre 2019',
+        content: (
+            <ul className='list-disc space-y-4 ml-5 text-s'>
+                <li>Desarrollo de front-end (ExtJS) y back-end (Java)</li>
+                <li>Mantenimiento de aplicaciones</li>
+                <li>Ejecución de pruebas</li>
+                <li>Desarrollo de documentación funcional</li>
+            </ul>
+        )
+    }
+];
+
 const ExperienceSection = () => {
   return (
     <motion.div 
@@ -15,10 +89,14 @@ const ExperienceSection = () => {
 
         <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory 
         scrollbar scrollbar-track-pink-500/20 scrollbar-thumb-pink-500/80'>
-            <ExperienceCard />
-            <ExperienceCard />
-            <ExperienceCard />
-            <ExperienceCard />
+            {DATA.map((d) => <ExperienceCard  key={d.id}
+            id={d.id}
+            src={d.src}
+            puesto={d.puesto}
+            empresa={d.empresa}
+            fechas={d.fechas}
+            content={d.content}
+            />)}
         </div>
     </motion.div>
   )
