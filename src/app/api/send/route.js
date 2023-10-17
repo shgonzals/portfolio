6bda17d953ceac172a765186beb45ac9dc8734ef
shgonzals <sh.gonzal.s@gmail.com>
2023-10-17 20:37:45 +0200

    update contact section

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,21 +1,28 @@
-//import { EmailTemplate } from '../../../components/EmailTemplate';
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+const fromEmail =  process.env.FROM_EMAIL;
+const toEmail = process.env.TO_EMAIL;
 
-export async function POST() {
+export async function POST(req, res) {
+  const body = await req.json();
+  const { email, subject, message } = body;
+  
   try {
     const data = await resend.emails.send({
-      from: 'Sheila <sh.gonzal.s@gmail.com>',
-      to: ['sh.gonzal.s@gmail.com'],
-      subject: 'Hello world',
-      react: <>
-                <p>Email body</p>
-            </>
+      from: fromEmail,
+      to: toEmail,
+      subject: subject,
+      react: (
+        <div>
+          <h2>{subject}</h2>
+          <p>{message}</p>
+          <p>Responder a: {email}</p>
+        </div>
+      )
     });
-
-    return NextResponse.json(data);
+    return NextResponse.json(data)
   } catch (error) {
     return NextResponse.json({ error });
   }
--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -1,66 +1,112 @@
-import React from 'react'
+"use client"
+import React from 'react';
 import GitHubIcon from '../../../public/images/github-icon.svg';
 import LinkedInIcon from '../../../public/images/linkedin-icon.svg';
 import Link from 'next/link';
 import Image from 'next/image';
 
 const EmailSection = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = {
+        email: e.target.email.value,
+        subject: e.target.subject.value,
+        message: e.target.message.value
+    };
+
+    const endpoint = "/api/send";
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data)
+    };
+
+    try {
+      const response = await fetch(endpoint, options);
+
+      if (response.status === 200) {
+        console.log('Message sent.');
+      } else {
+        console.error('Error sending the message.');
+      }
+    } catch (error) {
+      console.error('An error occurred:', error);
+    }
+  }
+
   return (
     <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 snap-center'>
-        <div>
-            <h5 className='top-24 uppercase tracking-[10px] font-semibold text-white text-2xl mb-4'>
-                Let&apos;s Connect
-            </h5>
-            <p className='text-[#ADB7BE] mb-4 max-w-md'>{" "}
-                I&apos;m currently looking for new opportunities, my inbox is always
-                open. Whether you have a question or just want to say hi, I&apos;ll
-                try my best to get back to you!
-          </p>
-          <div className='socials flex flex-row gap-2'>
-            <Link href="https://www.github.com/shgonzals" target="_blank">
-                <Image src={GitHubIcon} alt='GitHub Icon' />
-            </Link>
-            <Link href="https://www.linkedin.com/in/sh-gonzal" target="_blank">
-                <Image src={LinkedInIcon} alt='LinkedIn Icon' />
-            </Link>
+      <div>
+        <h5 className='top-24 uppercase tracking-[10px] font-semibold text-white text-2xl mb-4'>
+          Let&apos;s Connect
+        </h5>
+        <p className='text-[#ADB7BE] mb-4 max-w-md'>
+            Siempre estoy abierta a nuevas oportunidades, por lo que estaré encantada de o conectar a través de LinkedIn.
+            Si tienes alguna pregunta o simplemente quieres saludar, ¡intentaré responderte lo antes posible! :)
+        </p>
+        <div className='socials flex flex-row gap-2'>
+          <Link href="https://www.github.com/shgonzals" target="_blank">
+            <Image src={GitHubIcon} alt='GitHub Icon' />
+          </Link>
+          <Link href="https://www.linkedin.com/in/sh-gonzal" target="_blank">
+            <Image src={LinkedInIcon} alt='LinkedIn Icon' />
+          </Link>
+        </div>
+      </div>
+      <div>
+        <form className='flex flex-col' onSubmit={handleSubmit}>
+          <div className='mb-6'>
+            <label htmlFor='email' className='mb-2 text-white block text-sm font-medium'>
+              Email
+            </label>
+            <input
+              name='email'
+              type="email"
+              id="email"
+              className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
+              required
+              placeholder='Tu email'
+            />
           </div>
-        </div>
-        <div>
-            <form className='flex flex-col'>
-                <div className='mb-6'>
-                    <label htmlFor='email' type="email" className='mb-2 text-white block text-sm font-medium'>
-                        Email
-                    </label>
-                    <input type="email" id="email" 
-                    className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
-                    required placeholder='sh.gonzal.s@gmail.com'/>
-                    
-                </div>
-                <div className='mb-6'>
-                    <label htmlFor='subject' type="subject" className='mb-2 text-white block text-sm font-medium'>
-                        Asunto
-                    </label>
-                    <input type="text" id="subject" 
-                    className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
-                    required placeholder='Me gustaria contactar'/>
-                </div>
-                <div className='mb-6'>
-                    <label htmlFor='message' type="message" className='mb-2 text-white block text-sm font-medium'>
-                        Mensaje
-                    </label>
-                    <textarea type="text" id="message" 
-                    className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
-                    required placeholder='Deja tu mensaje aquí...'/>
-                </div>
-                <button 
-                type='submit'
-                className='bg-pink-500 hover:bg-pink-500 text-white font-medium py-2.5 px-5 rounded-lg w-full' >
-                    Enviar Email
-                </button>
-            </form>
-        </div>
+          <div className='mb-6'>
+            <label htmlFor='subject' className='mb-2 text-white block text-sm font-medium'>
+              Asunto
+            </label>
+            <input
+              name='subject'
+              type="text"
+              id="subject"
+              className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
+              required
+              placeholder='¡He visto tu perfil!'
+            ></input>
+          </div>
+          <div className='mb-6'>
+            <label htmlFor='message' className='mb-2 text-white block text-sm font-medium'>
+              Mensaje
+            </label>
+            <textarea
+              name='message'
+              id="message"
+              className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
+              required
+              placeholder='Me gustaría hablarte sobre...'
+            />
+          </div>
+          <button
+            type='submit'
+            className='bg-secondary-500 hover:bg-secondary-500 text-white font-medium py-2.5 px-5 rounded-lg w-full'
+          >
+            Enviar Email
+          </button>
+        </form>
+      </div>
     </section>
-  )
+  );
 }
 
-export default EmailSection
+export default EmailSection;
