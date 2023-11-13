45d710cc661e62a4fe0bc6abff05e7ab24a42a5c
shgonzals <sh.gonzal.s@gmail.com>
2023-11-13 20:49:56 +0100

    fix fromEmail resend

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail =  process.env.FROM_EMAIL;
+const fromEmail =  'Sheila González <' + process.env.FROM_EMAIL + '>';
 const toEmail = process.env.TO_EMAIL;
 
 export async function POST(req, res) {
